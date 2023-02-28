0ea1c20e5a47580dd87938d166866b5423d508e8
Oleg Danko <olegdanko@outlook.com>
2023-02-28 23:48:53 +0200

    Added Types utils, TypesChain

---
+++ include/utils/types/Types.hpp
@@ -0,0 +1,121 @@
+#pragma once
+
+#include <cstddef>
+#include <type_traits>
+#include <tuple>
+
+namespace utl_prf {
+
+template <typename... Ts>
+struct types {
+    using tpl = std::tuple<Ts...>;
+    static constexpr size_t count = std::tuple_size<tpl>();
+};
+
+template<typename, typename>
+struct cat_types;
+template<typename T, typename ...Ts>
+struct cat_types<T, types<Ts...>> {
+    using types_ = types<T, Ts...>;
+};
+template<typename T, typename ...Ts>
+struct cat_types<types<Ts...>, T> {
+    using types_ = types<Ts..., T>;
+};
+template<typename ...Ts1, typename ...Ts2>
+struct cat_types<types<Ts1...>, types<Ts2...>> {
+    using types_ = types<Ts1..., Ts2...>;
+};
+
+template<template<typename> typename, typename...>
+struct type_apply;
+template<template<typename> typename T, typename T1, typename...Ts>
+struct type_apply<T, T1, Ts...> {
+    using types_ = typename cat_types<T<T1>, typename type_apply<T, Ts...>::types_>::types_;
+};
+template<template<typename> typename T>
+struct type_apply<T> {
+    using types_ = types<>;
+};
+template<template<typename> typename T, typename ...Ts>
+struct type_apply<T, types<Ts...>> {
+    using types_ = typename type_apply<T, Ts...>::types_;
+};
+
+template<typename, typename...>
+struct is_unique;
+template<typename T>
+struct is_unique<T> { static constexpr bool val = true; };
+template<typename T, typename T1, typename ...Ts>
+struct is_unique<T, T1, Ts...> { static constexpr bool val = (!std::is_same<T, T1>::value) && is_unique<T, Ts...>::val; };
+template<typename T, typename... Ts>
+static constexpr bool is_unique_v = is_unique<T, Ts...>::val;
+
+template<typename, typename...>
+struct is_present;
+template<typename T>
+struct is_present<T> { static constexpr bool val = false; };
+template<typename T, typename T1, typename ...Ts>
+struct is_present<T, T1, Ts...> { static constexpr bool val = (std::is_same<T, T1>::value) || is_present<T, Ts...>::val; };
+
+template<typename T, typename ...Ts>
+struct is_present<T, types<Ts...>> { static constexpr bool val = is_present<T, Ts...>::val; };
+
+
+template<typename T, typename... Ts>
+static constexpr bool is_present_v = is_present<T, Ts...>::val;
+
+
+template<typename ... Ts>
+struct unique_types {
+    template<typename C, typename ...As>
+    struct collector;
+    template<typename CollectedPrev, typename A>
+    struct collector<CollectedPrev, A> {
+        using types_ = typename std::conditional<!is_present_v<A, CollectedPrev>,
+                                                 typename cat_types<CollectedPrev, A>::types_,
+                                                 CollectedPrev>::type;
+    };
+    template<typename UNIQUES_PREV, typename A, typename ...As>
+    struct collector<UNIQUES_PREV, A, As...> {
+        using uniques = typename collector<UNIQUES_PREV, A>::types_;
+        using types_ = typename collector<uniques, As...>::types_;
+    };
+public:
+    using types_ = typename collector<types<>, Ts...>::types_;
+};
+
+template<std::size_t I>
+struct size_ {
+    static constexpr std::size_t val = I;
+};
+
+template<typename T, typename ...Ts>
+struct index_of {
+    template<typename A, typename ...As>
+    struct index_of_;
+    template<typename A>
+    struct index_of_<A> {
+        static constexpr size_t i = 0;
+    };
+    template<typename A, typename A1, typename ...As>
+    struct index_of_<A, A1, As...> {
+        static constexpr size_t i =
+                std::conditional<std::is_same_v<A, A1>,
+                                 size_<0>,
+                                 size_<1 + index_of_<A, As...>::i>>::type::val;
+    };
+
+    static_assert(is_present_v<T, Ts...>, "In index_of, type is not found in the type list");
+    static constexpr size_t i = index_of_<T, Ts...>::i;
+};
+
+template<typename T, typename ...Ts>
+struct index_of<T, types<Ts...>> {
+    static constexpr size_t i = index_of<T, Ts...>::i;
+};
+
+template<typename T, typename ...Ts>
+constexpr size_t index_of_v = index_of<T, Ts...>::i;
+
+}
---
+++ include/utils/types/TypesChain.hpp
@@ -0,0 +1,106 @@
+#pragma once
+
+#include "Types.hpp"
+
+namespace utl_prf {
+
+template<typename ...Ts>
+struct TypesChain;
+
+template<>
+struct TypesChain<> {};
+
+template<typename T>
+struct TypesChain<T> {
+    T arg;
+
+    TypesChain() = default;
+    TypesChain(const T& a) : arg(a) {}
+    TypesChain(T&& a) : arg{std::move(a)} {}
+    TypesChain(TypesChain&& chain) : arg{std::move(chain.arg)} {}
+    TypesChain(const TypesChain& chain) : arg{chain.arg} {}
+
+    template<typename A>
+    A& get() {
+        return arg;
+    }
+    template<typename A>
+    const A& get() const {
+        return arg;
+    }
+};
+
+template<typename T, typename ...Ts>
+struct TypesChain<T, Ts...> : TypesChain<T> {
+    static_assert(is_unique_v<T, Ts...>, "Types in TypeChain must be unique");
+    using Base_t = TypesChain<T>;
+    TypesChain<Ts...> chain;
+
+    TypesChain() = default;
+    TypesChain(const T& a, const Ts&... args) : Base_t(a), chain(args...) {}
+    TypesChain(T&& a, Ts&&... args) : Base_t(std::move(a)), chain(std::move(args)...) {}
+
+    TypesChain(TypesChain&& that)
+        : Base_t(std::move(static_cast<Base_t>(that).arg)),
+          chain(std::move(that.chain)) {}
+
+    TypesChain(const TypesChain& that)
+        : Base_t(static_cast<Base_t>(that)),
+          chain(std::move(that.chain)) {}
+
+    template<typename A>
+    A& get() {
+        if constexpr (std::is_same<A, T>::value) {
+            return Base_t::arg;
+        } else {
+            return chain.template get<A>();
+        }
+    }
+    template<typename A>
+    const A& get() const {
+        if constexpr (std::is_same<A, T>::value) {
+            return Base_t::arg;
+        } else {
+            return chain.template get<A>();
+        }
+    }
+};
+
+template<typename ...Ts>
+TypesChain<Ts...> make_types_chain(Ts&& ... args) {
+    return TypesChain<Ts...>(std::move(args)...);
+}
+
+template<> inline TypesChain<> make_types_chain<>() {
+    return {};
+}
+
+template<template<typename >typename Arg, typename ...Ts>
+struct SubTypesChain;
+
+template<template<typename >typename Arg>
+struct SubTypesChain<Arg> {};
+
+template<template<typename >typename Arg, typename ...Ts>
+struct SubTypesChain : private TypesChain<Arg<Ts>...> {
+    SubTypesChain() = default;
+    SubTypesChain(Arg<Ts>&&...args) : TypesChain<Arg<Ts>...>(std::move(args)...) {}
+    SubTypesChain(SubTypesChain&& that) : TypesChain<Arg<Ts>...>(std::move(that)) {}
+    SubTypesChain(const SubTypesChain& that) : TypesChain<Arg<Ts>...>(that) {}
+
+    template<typename T>
+    Arg<T>& get() {
+        return TypesChain<Arg<Ts>...>::template get<Arg<T>>();
+    }
+    template<typename T>
+    const Arg<T>& get() const {
+        return TypesChain<Arg<Ts>...>::template get<Arg<T>>();
+    }
+};
+
+template<template<typename >typename Arg, typename ...Ts>
+auto make_subtypes_chain(Arg<Ts>&&... args) {
+    return SubTypesChain<Arg, Ts...>(std::move(args)...);
+}
+
+}
